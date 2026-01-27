316f9e18d284357bb3b856b68bb1b8b8b6d302ec
kun7l <kunalkhairanar007@gmail.com>
2026-01-27 11:41:04 +0530

    Day-6 coding done

---
+++ Day-6/Async-Programming.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day_6
+{
+    internal class Async_Programming
+    {
+        public async Task<string> getData(int id) {
+            Console.WriteLine("Fetching started Asynchronously");
+            await Task.Delay(3000);
+            Console.WriteLine("Fetching completed");
+            return "_Krunalll7";
+        }
+        public string getDataSync(int id)
+        {
+            Console.WriteLine("Fetching started synchronously");
+            Thread.Sleep(3000);
+            Console.WriteLine("Fetching completed");
+            return "_Krunalll7";
+        }
+        static async Task Main(string[] args)
+        {
+
+            Async_Programming ap = new Async_Programming();
+            try
+            {
+                string? userName;
+
+                //Get data with sync
+                Stopwatch normalTimer = Stopwatch.StartNew();
+                userName = ap.getDataSync(12);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("performing other tasks");
+                    await Task.Delay(1000);
+                }
+                Console.WriteLine("Response :" + userName);
+
+                normalTimer.Stop();
+                Console.WriteLine($"Sync Total Runtime (ms): {normalTimer.ElapsedMilliseconds} ms");
+
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                //Get data with async
+
+                Stopwatch asyncTimer = Stopwatch.StartNew();
+                Task<string> apiTask = ap.getData(12);
+                for (int i = 0; i < 5; i++)
+                {
+                    await Task.Delay(1000);
+                    Console.WriteLine("performing other tasks");
+
+                }
+                Console.WriteLine("Response : " + apiTask.Result);
+
+                asyncTimer.Stop();
+                Console.WriteLine($"Async Total Runtime (ms): {asyncTimer.ElapsedMilliseconds} ms");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+}
---
+++ Day-6/Concurrent_collection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_6
+{
+    internal class Concurrent_collection
+    {
+        List<int> list = new List<int>();
+        ConcurrentBag<int> bag = new ConcurrentBag<int>();
+        
+        public void AddSetA()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                list.Add(i);
+                bag.Add(i);
+                Thread.Sleep(1);
+            }
+        }
+        public void AddSetB()
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                list.Add(i);
+                bag.Add(i);
+                Thread.Sleep(1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Concurrent_collection cc = new Concurrent_collection();
+            cc.list.Add(0);
+           
+            Thread t1 = new Thread(cc.AddSetA);
+            Thread t2 = new Thread(cc.AddSetB);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            int sumList = 0;
+            int sumConBag = 0;
+            foreach(var item in cc.list)
+            {
+                sumList += item;
+            }
+            foreach(var item in cc.bag)
+            {
+                sumConBag += item;
+            }
+            Console.WriteLine("By logic Adding 1-10 2 times result in total of 110");
+            Console.WriteLine("If you get 110 in both, run again");
+            Console.WriteLine("Sum of List " + sumList);
+            Console.WriteLine("Sum of Concurrent Bag " + sumConBag);
+        }
+    }
+}
