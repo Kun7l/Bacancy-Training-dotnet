c0bfdbf783882c21bf93dfece570e8a85f263a66
kun7l <kunalkhairanar007@gmail.com>
2026-02-22 18:55:13 +0530

    Model-configuration added

---
+++ EF-Core/Corporate-training-management-system/Repository/Models/Configuration/TrainerConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corporate_training_management_system.Repository.Models.Configuration
+{
+    internal class TrainerConfiguration
+    {
+    }
+}
