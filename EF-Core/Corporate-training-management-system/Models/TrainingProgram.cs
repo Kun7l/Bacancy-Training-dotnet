4229909a28842fbc8b92d083dde0f637072c339d
kun7l <kunalkhairanar007@gmail.com>
2026-02-22 16:13:13 +0530

    Dependencies downloded and Models created

---
+++ EF-Core/Corporate-training-management-system/Models/TrainingProgram.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corporate_training_management_system.Models
+{
+    internal class TrainingProgram
+    {
+    }
+}
