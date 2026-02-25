57a161d026da8a425e48861bd0f55633a58f9de3
kun7l <kunalkhairanar007@gmail.com>
2026-02-25 09:41:37 +0530

    removed service folder

--- .NET Core/Product-Catalog-Api/Services/IProductServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using Product_Catalog_Api.Repository.Model;
-
-namespace Product_Catalog_Api.Services
-{
-    public interface IProductServices 
-    {
-        public List<Product> GetAllProducts(int id);
-    }
-}
